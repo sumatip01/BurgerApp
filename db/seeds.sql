ae7948d97fa1315e15d26d2d652f7f4aef9a4641
sumati <sumati.patel@hotmail.com>
2018-09-14 13:11:41 -0400

    db done

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('veggie');
+INSERT INTO burgers (name) VALUES ('bison');
+INSERT INTO burgers (name, cheese) VALUES ('ham', true);
+INSERT INTO burgers (name, cheese) VALUES ('bacon', true);
+INSERT INTO burgers (name) VALUES ('chicken');
