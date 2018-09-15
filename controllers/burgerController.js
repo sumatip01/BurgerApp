200d34b788d3133f0a60b12928d7aacd9657288f
sumati <sumati.patel@hotmail.com>
2018-09-15 13:27:30 -0400

    fixing errors

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 
-var burger = require("../models/burger.js");
+var burger = require("../models/burgers.js");
 
 router.get("/", function(req, res) {
   burgers.all(function(data) {
--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar (255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	cheese BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
